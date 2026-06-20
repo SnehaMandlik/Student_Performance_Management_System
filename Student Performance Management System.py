8743e3cb3ea7ca5d804d3b8a62ed73be3c0b560e
Sneha Mandlik <snehamandlik1106@gmail.com>
2026-06-20 20:40:35 +0530

    Update Student Performance Management System.py

--- Student Performance Management System.py
+++ Student Performance Management System.py
@@ -1,16 +1,16 @@
-# n = int(input("Enter number of students: "))
-# students = {}
+ n = int(input("Enter number of students: "))
+ students = {}
 
-# for i in range(n):
-#     name = input("\nEnter student name: ")
-#     marks = []
-#     for j in range(3):  # 3 subjects
-#         marks.append(int(input(f"Enter marks for subject {j+1}: ")))
-#     students[name] = marks
+ for i in range(n):
+     name = input("\nEnter student name: ")
+     marks = []
+     for j in range(3):  # 3 subjects
+         marks.append(int(input(f"Enter marks for subject {j+1}: ")))
+     students[name] = marks
 
-# print("\n--- Student Marksheet ---")
-# for name, marks in students.items():
-#     total = sum(marks)
-#     avg = total / len(marks)
-#     print(f"{name} => Marks: {marks}, Total: {total}, Average: {avg:.2f}")
+ print("\n--- Student Marksheet ---")
+ for name, marks in students.items():
+     total = sum(marks)
+     avg = total / len(marks)
+     print(f"{name} => Marks: {marks}, Total: {total}, Average: {avg:.2f}")
 
